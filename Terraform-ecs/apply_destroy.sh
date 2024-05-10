a72af578f743f8d1fb6c50beb34a246e672364ad
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-05-10 16:34:26 +0530

    Update apply_destroy.sh

--- Terraform-ecs/apply_destroy.sh
+++ Terraform-ecs/apply_destroy.sh
@@ -3,6 +3,9 @@
 # Prompt user for input
 read -p "Do you want to apply or destroy infrastructure? (apply/destroy): " choice
 
+# Convert user input to lowercase
+choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+
 # Validate user input
 if [[ "$choice" == "apply" ]]; then
     echo "Applying infrastructure..."
