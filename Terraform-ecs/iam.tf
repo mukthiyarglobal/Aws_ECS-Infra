cb90a0fbb73e06ce4a0fff129a64b50b1b74629f
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-05-29 15:43:43 +0530

    Update iam.tf
    IAM file commited

--- Terraform-ecs/iam.tf
+++ Terraform-ecs/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "ecsTaskExecutionRole" {
+#resource "aws_iam_role" "ecsTaskExecutionRole" {
 #   name               = var.ecs_task_execution_role
 #   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 # }
