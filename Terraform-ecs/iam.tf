b1d43abbe46f87b22c3170a8c9eb2acbe731b20c
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-05-29 15:41:53 +0530

    Update iam.tf

--- Terraform-ecs/iam.tf
+++ Terraform-ecs/iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  name               = var.ecs_task_execution_role
-  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-}
+#   name               = var.ecs_task_execution_role
+#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+# }
 
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
+# data "aws_iam_policy_document" "assume_role_policy" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-  role       = aws_iam_role.ecsTaskExecutionRole.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+# resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+#   role       = aws_iam_role.ecsTaskExecutionRole.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
